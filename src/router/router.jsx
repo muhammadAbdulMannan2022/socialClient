35ca3d1d86580037945dabd60db03af553c6bfd4
muhammad <programalltest@gmail.com>
2024-10-10 10:41:00 +0600

    error route done

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Link } from "react-router-dom";
 import HomeLayout from "../layouts/HomeLayout";
 import Home from "../components/Home/Home";
 import LogIn from "../components/LogIn/LogIn";
@@ -36,6 +36,22 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignUp />,
+  },
+  {
+    path: "*",
+    element: (
+      <div className="bg-black text-white w-full h-[100vh] flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold md:text-4xl lg:text-6xl">
+          404 NOT FOUND
+        </h1>
+        <span>
+          Go to{" "}
+          <Link to="/" className="underline text-blue-500">
+            home
+          </Link>
+        </span>
+      </div>
+    ),
   },
 ]);
 
